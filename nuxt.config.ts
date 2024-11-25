d16e952360ee877e7654f8f719b868afabb0afd2
Utitofon-Udoekong <utitofonudoekong0@gmail.com>
2024-11-25 17:41:01 +0100

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 export default defineNuxtConfig({
   // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
-  devtools: { enabled: true },
-
-  modules: [
-  ],
-  compatibilityDate: '2024-10-24'
+  devtools: { enabled: false },
+  compatibilityDate: '2024-10-24',
 })
